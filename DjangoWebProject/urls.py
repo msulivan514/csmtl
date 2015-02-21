98026209b76819f82b712b0e9d1b58b5242b4130
thibaud.comte <thibaud.comte@yahoo.ca>
2015-02-21 16:55:47 -0500

    Add new form to index.html; Add jscript to get form values from json; Add new searchFilters view into Django.

--- DjangoWebProject/urls.py
+++ DjangoWebProject/urls.py
@@ -8,6 +8,7 @@ from app.forms import BootstrapAuthenticationForm
 
 urlpatterns = patterns('',
     url(r'^$', 'app.views.home', name='home'),
+    url(r'^searchFilers$', 'app.views.filters', name='searchFilters'),
     url(r'^contact$', 'app.views.contact', name='contact'),
-    url(r'^about', 'app.views.about', name='about'),
+    url(r'^about$', 'app.views.about', name='about'),
 )
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ Definition of views.
 """
 
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.template import RequestContext
 from datetime import datetime
 
@@ -19,6 +19,44 @@ def home(request):
             'year':datetime.now().year,
         })
     )
+
+def filters(request):
+    """Builds Json file with search filters"""
+    assert isinstance(request, HttpRequest)
+
+    filters = dict()
+
+    fields = dict()
+    fields[0] = "field1"
+    fields[1] = "field2"
+    fields[2] = "field3"
+
+    grades = dict()
+    grades[0] = "grade1"
+    grades[1] = "grade2"
+    grades[2] = "grade3"
+
+    provinces = dict()
+    provinces[0] = "province1"
+    provinces[1] = "province2"
+    provinces[2] = "province3"
+
+    ages = dict()
+    ages[0] = "12-17"
+    ages[1] = "18-24"
+    ages[2] = "25-45"
+
+    sex = dict()
+    sex[0] = "male"
+    sex[1] = "female"
+    
+    filters["fields"] = fields
+    filters["grades"] = grades
+    filters["provinces"] = provinces
+    filters["ages"] = ages
+    filters["sex"] = sex
+    
+    return JsonResponse(filters)
 
 def contact(request):
     """Renders the contact page."""
